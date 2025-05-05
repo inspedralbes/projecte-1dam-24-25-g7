c81203e7b1b5858502d8ef13a8be4443e6c6980a
a24judsarand <a24judsarand@inspedralbes.cat>
2025-05-05 09:12:11 +0200

    Tu mensaje

---
+++ GestorIncidencies/src/models/Actuacions.js
@@ -0,0 +1,45 @@
+// src/models/Actuacions.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const Actuacions = sequelize.define('Actuacions', {
+  id_resolution: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  resolutionDate: {
+    type: DataTypes.DATE,
+    allowNull: true, 
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false, // Una descripció detallada és necessària
+  },
+  investedTime: { // temps dedicat a resoldre la incidencia
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  ActuacioResolution: { // resolucion
+    type: DataTypes.String,
+    allowNull: false,
+  },
+  idTecnic: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+
+
+}, {
+  tableName: 'actuacions',
+  timestamps: true,
+});
+
+// Associacions
+// Incident.belongsTo(User, { foreignKey: 'reporterUserId', as: 'reporter' });
+// Incident.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser' });
+// Incident.belongsTo(Status, { foreignKey: 'statusId' });
+// Incident.belongsTo(Priority, { foreignKey: 'priorityId' });
+// Incident.hasMany(Comment, { foreignKey: 'incidentId' });
+
+module.exports = Actuacions;
